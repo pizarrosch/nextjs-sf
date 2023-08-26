8248b343dbf56ec1237551476efe9741c40ff7dc
Zaur Shomakhov <murtukov@gmail.com>
2023-08-26 23:20:40 +0200

    Add styles to the ident file content

--- src/pages/portfolioID/[id]/ident.tsx
+++ src/pages/portfolioID/[id]/ident.tsx
@@ -3,6 +3,7 @@ import Layout from "@/components/layout";
 import Image from 'next/image';
 import Link from "next/link";
 import axios from "axios";
+import s from './ident.module.scss';
 
 interface ItemProps {
     portfolioItem: PortfolioItem;
@@ -14,7 +15,6 @@ export async function getServerSideProps({ params }) {
   const url = `${PHOTOS_API_URL}/${params.id}`
   const { data } = await axios.get<PortfolioItem[]>(url);
 
-
   return {
     props: { portfolioItem: data }
   }
@@ -23,14 +23,16 @@ export async function getServerSideProps({ params }) {
 export default function Item({ portfolioItem }: ItemProps) {
     return (
         <Layout>
-            <div>
-                <h1>Проект: {portfolioItem.title}</h1>
+            <div className={s.root}>
+                <h1 className={s.title}>Проект: {portfolioItem.title}</h1>
                 <Image
                     src={portfolioItem.url}
                     width={400}
                     height={400}
                     alt={portfolioItem.title} />
-                <Link href="/portfolio">Назад к списку работ</Link>
+                <Link href="/portfolio">
+                  <div className={s.returnButton}>Назад к списку работ</div>
+                </Link>
             </div>
         </Layout>
     )
